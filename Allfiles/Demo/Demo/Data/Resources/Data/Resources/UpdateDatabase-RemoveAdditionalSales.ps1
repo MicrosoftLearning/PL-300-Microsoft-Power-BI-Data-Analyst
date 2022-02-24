dd4753d08c869d3e4e2a96c72ac9f85acfc31dbe
Brian Moring <87440652+bpmoring@users.noreply.github.com>
2022-02-24 10:58:38 -0800

    Delete Allfiles/Demo/Demo/Data/Resources/Data directory

--- Allfiles/Demo/Demo/Data/Resources/Data/Resources/UpdateDatabase-RemoveAdditionalSales.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host -foreground green 'Updating database...';
-Write-Host 'Removing supplementary sales data...';
-
-Invoke-SqlCmd -Server DATA-AI -Database "TailspinToys2020-US" -Query "EXEC [dbo].[uspSales_LoadData_Undo];"
-
-Write-Host 'Press any key to close...';
-$z = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
